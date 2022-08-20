69fc6963e789be4b8999615a1cab915b8d0ea1de
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-08-20 14:13:47 +0000

    added abit functionality to validating user registration inputs

--- main_code/functions_for_main/components/registration_form.py
+++ main_code/functions_for_main/components/registration_form.py
@@ -1,19 +1,30 @@
 from functions_for_main.functions import mtn_MOMO_Registered_user
-from functions_for_main.functions import mtn_MOMO_Registered_user_pin 
+from functions_for_main.functions import mtn_MOMO_Registered_user_pin
+from functions_for_main.functions import no_int_begins 
 
 print("\n")
 print("A test of MTN GH MOMO Services..... \n")
 
 regUser = input("Enter your name to register: ")
-new_applicant = mtn_MOMO_Registered_user(regUser)
+reguser = no_int_begins(regUser)
+
+if regUser == '':
+    print("Did you entered spacebar or didnt, Man you are sick, start the process again")
+    exit()
+else:
+    new_applicant = mtn_MOMO_Registered_user(reguser)
 
 regUserPin = input("Enter your preferred 4 - digit pin: ")
-new_applicant_pin = mtn_MOMO_Registered_user_pin(regUserPin)
+if regUserPin == ' ':
+    print("The field is empty, retart the process again")
+else:    
+    new_applicant_pin = mtn_MOMO_Registered_user_pin(regUserPin)
 
 re_regUserPin = input("Enter your pin_number again:  ")
 
 if re_regUserPin == regUserPin:
     print("Y'ello {}!, you have been registered successfully!".format(regUser))
+    
     
 else:
     print("Sorry you entered a wrong password for your pin registration, try again")
--- main_code/functions_for_main/functions.py
+++ main_code/functions_for_main/functions.py
@@ -1,11 +1,24 @@
+import numpy as np
 
-
-def mtn_MOMO_Registered_user(registeredUser):
+def mtn_MOMO_Registered_user(splitter):
+    index_size_counter = 0
     regUser = []
-    regUser.append(registeredUser)
-    return regUser
-
+    regUser.append(splitter)
+    index_size_counter+1
+        
 def mtn_MOMO_Registered_user_pin(pin_number):
+    index_size = 10
     regUserPin = []
-    regUserPin.append(pin_number)
+    for i in range(index_size):
+        regUserPin.append(pin_number)
     return regUserPin
+
+def no_int_begins(registeredUser):
+    splitter =[registeredUser]
+    bugger = [char for char in splitter[0]]
+    size_splitter_carries = len(bugger)
+    for i in range(size_splitter_carries):
+        if bugger[0] == '0 - 9':
+            print("started with a number")
+        else:
+            return splitter
